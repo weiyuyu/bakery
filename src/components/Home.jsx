7336305bfbb027a6eb8d9487dd30b52e2ed03d4b
Wei-Yu (Wayne) Yu <weiyuyu@usc.edu>
2018-07-20 01:53:48 +0800

    Completed landing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,33 +1,81 @@
 import React from 'react';
-import { Container, Grid } from 'semantic-ui-react';
-import ReactPlaceholder from 'react-placeholder';
-import "react-placeholder/lib/reactPlaceholder.css";
+import { Container, Grid, Image, Divider } from 'semantic-ui-react';
+import Fade from 'react-reveal/Fade';
+import Zoom from 'react-reveal/Zoom';
+import logo from './../img/伯爵茶司康/CE8AFA52-06CB-403C-A8C9-07DCFBFD2FCC.JPG';
 
-const Home = () => {
-  const { containerStyle, gridStyle } = styles;
-  return (
-    <Container style={containerStyle}>
-      <Grid columns={2} divided style={gridStyle}>
-        <Grid.Row>
-          <Grid.Column>
-            <ReactPlaceholder showLoadingAnimation color='#ffaeae' type='media' rows={7} ready={false} />
-          </Grid.Column>
-          <Grid.Column>
-            <ReactPlaceholder showLoadingAnimation color='#ffaeae' type='media' rows={7} ready={false} />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Container>
-  );
-};
+export default class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = { show: false };
+  }
 
-const styles = {
-  containerStyle: {
-    'padding': 20
-  },
-  gridStyle: {
-    marginTop: 50
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ show: true });
+    }, 2000);
+  }
+
+  render() {
+    const { containerStyle, gridStyle, introStyle, paragraphStyle, imageStyle } = styles;
+    return (
+      <Container>
+        <Grid stackable columns={2} style={gridStyle}>
+          <Grid.Row>
+            <Grid.Column style={containerStyle}>
+              <Zoom>
+                <Image src={logo} style={imageStyle} size='medium' rounded/>
+              </Zoom>
+            </Grid.Column>
+            <Divider />
+            <Grid.Column style={containerStyle}>
+              <div style={introStyle}>
+                <Fade left>
+                  <h1>
+                    Janet’s Bakery <br/> 成立於2018年
+                  </h1>
+                  <p style={{'fontSize': 25}}> / </p>
+                </Fade>
+                <Fade right>
+                  <div>
+                    <h2 style={paragraphStyle}>
+                      用為女兒烘焙的雙手<br/>
+                      秉持著初衷與熱情<br/>
+                      嚴選最天然、頂級的素材<br/>
+                      呈現出最健康的美味
+                    </h2>
+                  </div>
+                </Fade>
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    );
   }
 }
 
-export default Home;
+const styles = {
+  containerStyle: {
+    'padding': 0,
+    'display': 'flex',
+    'justifyContent': 'center'
+  },
+  gridStyle: {
+    marginTop: 30
+  },
+  introStyle: {
+    'marginTop': 'auto',
+    'marginBottom': 'auto',
+    'textAlign' : 'left'
+  },
+  paragraphStyle: {
+    'fontWeight': 500,
+  },
+  imageStyle: {
+    'display': 'block',
+    'margin': 'auto',
+    'width': '80%',
+    'height': 'auto'
+  }
+}
