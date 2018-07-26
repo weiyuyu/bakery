25c46d6d59113236f53e9ac4ba2440adca27759b
Wei-Yu (Wayne) Yu <weiyuyu@usc.edu>
2018-07-26 12:15:40 +0800

    Added email sending

--- src/components/CustomerForm.js
+++ src/components/CustomerForm.js
@@ -39,7 +39,7 @@ const pickupOptions = [
   }
 ];
 
-const pickupTimeOptions = [
+const shippingTimeOptions = [
   {
     text: '早上 (13:00前)｜Morning',
     value: 'morning'
@@ -60,10 +60,13 @@ export default class CustomerForm extends React.Component {
     this.state = {
       name: null,
       email: null,
+      phone: null,
+      comments: null,
+      shippingTime: null,
       loading: false,
       success: false,
-      pickupSelected: false,
-      shippingSelected: false
+      error: false,
+      shippingSelected: null
     }
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleNameInput = this.handleNameInput.bind(this);
@@ -72,35 +75,83 @@ export default class CustomerForm extends React.Component {
 
   handleNameInput = (e) => {
     this.setState({name: e.target.value});
-  }
+  };
 
   handleEmailInput = (e) => {
     this.setState({email: e.target.value});
-  }
+  };
+
+  handlePhoneInput = (e) => {
+    this.setState({phone: e.target.value});
+  };
+
+  handleCommentsInput = (e) => {
+    this.setState({comments: e.target.value});
+  };
 
   handlePickupDropdownChange = (e, selection) => {
     switch(selection.value) {
     case 'pickup':
-      this.setState({pickupSelected: true});
       this.setState({shippingSelected: false});
       break;
     case 'shipping':
-      this.setState({pickupSelected: false});
       this.setState({shippingSelected: true});
       break;
     default:
       break;
     }
-  }
+  };
 
-  handleSubmit = () => {
-    if(this.state.name && this.state.email) {
+  handleShippingTimeDropdownChange = (e, selection) => {
+    this.setState({shippingTime: selection.value});
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    let formCompleted = true;
+    if(!this.state.name || !this.state.email || !this.state.phone) {
+      formCompleted = false;
+    }
+    if(this.state.shippingSelected === null && this.state.pickupSelected === null) {
+      formCompleted = false
+    }
+
+    if(this.state.shippingSelected === true && this.state.shippingTime === null) {
+      formCompleted = false;
+    }
+
+    if(formCompleted) {
+      this.setState({error: false});
       this.setState({loading: true});
+      this.sendEmail('confirmation_email', this.state.email, this.state.name, '$800*1 + $350*1', 220, 1370);
+    } else {
+      this.setState({error: true});
     }
   };
 
+  sendEmail(templateId, email, customerName, costString, shippingCost, totalCost) {
+    window.emailjs.send(
+      'default_service',
+      templateId,
+      {
+        email,
+        customerName,
+        costString,
+        shippingCost,
+        totalCost
+      })
+      .then(res => {
+        this.setState({loading: false});
+        this.setState({success: true});
+        console.log('Email sent');
+      })
+      // Handle errors here however you like, or use a React error boundary
+      .catch(err => console.error('Failed to send email. Error: ', err))
+  }
+
   render() {
-    const { loading, success, error, pickupSelected, shippingSelected } = this.state;
+    const { loading, success, error, shippingSelected } = this.state;
     const { formStyle, messageStyle, buttonStyle, formGroupStyle } = styles;
     return (
       <Form loading={loading} success={success} error={error} style={formStyle}>
@@ -109,18 +160,24 @@ export default class CustomerForm extends React.Component {
           <Form.Input required label='Email' placeholder='janetsbakery@gmail.com' onChange={this.handleEmailInput}/>
           <Form.Dropdown id="pickupDropdown" required label='取貨方式' placeholder='取貨方式' options={pickupOptions} onChange={this.handlePickupDropdownChange}/>
         </Form.Group>
-        {
-          (shippingSelected) &&
-          <Form.Group widths='equal' style={formGroupStyle}>
-            <Form.Input required label='聯絡電話' placeholder='0912-345-678' onChange={this.handlePhoneInput}/>
-            <Form.Dropdown id="pickupTimeDropdown" required label='宅配時間' placeholder='宅配時間' options={pickupTimeOptions} onChange={this.handlePickupTimeDropdownChange}/>
-          </Form.Group>
-        }
+        <Form.Group widths='equal' style={formGroupStyle}>
+          <Form.Input required label='聯絡電話' placeholder='0912-345-678' onChange={this.handlePhoneInput}/>
+          {
+            (shippingSelected) && <Form.Dropdown id="shippingTimeDropdown" required label='宅配時間' placeholder='宅配時間' options={shippingTimeOptions} onChange={this.handleShippingTimeDropdownChange}/>
+          }
+          <Form.Input label='備註/其他' placeholder="想對Janet's Bakery說些什麼？" onChange={this.handleCommentsInput}/>
+        </Form.Group>
         <Button style={buttonStyle} onClick={this.handleSubmit}>Submit</Button>
         <Message
           success
           header='Order Completed'
           content="We've placed your order! Check your inbox for a confirmation email."
+          style={messageStyle}
+        />
+        <Message
+          error
+          header='Fields Incomplete'
+          content="Please fill in all required fields."
           style={messageStyle}
         />
       </Form>
